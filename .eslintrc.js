ca5b73a32f4369c820484c089dff11ee96a5eef8
Resi Respati <resir014@gmail.com>
2020-07-22 14:40:30 +0700

    fix: split eslint-import-resolver-typescript from config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['./index.js', 'eslint-config-prettier', 'eslint-config-prettier/@typescript-eslint'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
