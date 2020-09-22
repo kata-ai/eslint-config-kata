1494eac59988254e20ca3a63f0ffead153d6ddc7
Resi Respati <resir014@gmail.com>
2020-09-22 11:13:55 +0700

    fix: don't set `ecmaVersion` directly in the config (#7)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['./index.js', 'eslint-config-prettier', 'eslint-config-prettier/@typescript-eslint'],
   plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
   rules: {
     'prettier/prettier': 'error',
   },
